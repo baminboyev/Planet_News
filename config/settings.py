a995b54d122138ad22e95d84954104a8a577e651
Behruz Aminboyev <behruzaminboyev01@gmail.com>
2023-01-23 18:12:54 +0300

    delete_env

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,4 @@
 from pathlib import Path
-from environs import Env
-
-#Enviroment variables
-
-env = Env()
-env.read_env()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -15,10 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = ("django-insecure-(!p5mms2222u(x369+q82ay-iyo%5)@cqxiu!#x_$$bil20-%h")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default = False)
+DEBUG = False
 
 ALLOWED_HOSTS = ['.pythonanywhere.com','https://baminboyev.uz','www.baminboyev.uz','127.0.0.1']
 
@@ -79,8 +72,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default" : env.dj_db_url("DATABASE_URL")
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
